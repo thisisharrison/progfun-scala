d48841e811d54a3f95847f78517b8533f11fdd95
Harrison Lau <harrison.lau@outlook.com>
2021-11-20 21:52:23 +0800

    week3 lecture 3.5

---
+++ src/main/scala/week3/Rational.scala
@@ -0,0 +1,42 @@
+package week3
+
+import scala.annotation.tailrec
+
+class Rational(x: Int, y: Int) {
+  def this(x: Int) = this(x, 1)
+
+  require(y != 0, "denominator must be nonzero")
+
+  @tailrec
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+  private val g = gcd(x, y)
+
+  def numer: Int = x / g
+  def denom: Int = y / g
+
+  def +(r: Rational) =
+    new Rational((r.denom * numer + r.numer * denom), (denom * r.denom))
+
+  def unary_- : Rational =
+    new Rational(-numer, denom)
+
+  def - (r: Rational) =
+    this + -r
+
+  def * (r: Rational) =
+    new Rational(r.numer * numer, r.denom * denom)
+
+  def / (r: Rational) =
+    this * new Rational(r.denom, r.numer)
+
+  def == (r: Rational): Boolean =
+    r.denom * numer == denom * r.numer
+
+  def < (r: Rational): Boolean = numer * r.denom < r.numer * denom
+
+  def max(r: Rational): Rational = if (this < r) r else this
+
+  override def toString: String =
+    numer + "/" + denom
+}
+
---
+++ src/week3/3.5-lecture.sc
@@ -0,0 +1,19 @@
+//import week3.Rational
+// import everything from package
+import week3._
+// import week3.{Rational. Hello} // <- named imports
+
+object week3_5 {
+  new week3.Rational(3, 5)
+
+//  imported and we don't need package.ClassName
+  new Rational(1, 2)
+
+//  Null type
+  val x: String = null
+  val y: String = x
+//  val z: Int = null // <- an expression of type Null is ineligible for implicit conversion
+
+//  AnyVal
+  if (true) 1 else false // AnyVal 1 because Int and Boolean belongs to AnyVal and type checker picked the type that matched both
+}
